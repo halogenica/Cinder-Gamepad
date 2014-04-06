8f3a159f7944b2e50c101e53f8e0f3993c11b01c
angelkatalyst <angelkatalyst@users.noreply.github.com>
2014-04-05 23:47:32 -0700

    Initial upload of gamepad block
    Initial upload of gamepad block

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Cinder-Gamepad
-==============
+This is a port of Alex Diener's cross platform gamepad library to Cinder.
+The block supports Windows (vs2012) and Mac. Original linux code is included.
 
-Gamepad block for Cinder framework (libcinder.org)
+Gamepad Version 1.3.0
+
+Original forum post:
+http://forums.tigsource.com/index.php?topic=10675
+
+Original SVN repo:
+http://sacredsoftware.net/svn/misc/StemLibProjects/gamepad/tags/1.3.0/
+
+License is included.
---
+++ samples/GamepadTest/src/GamepadTest.cpp
@@ -0,0 +1,109 @@
+#include "cinder/app/AppBasic.h"
+
+// Gamepad block is a C library, so must use extern "C" to include from C++
+extern "C" {
+#include "Gamepad.h"
+}
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+static bool verbose = true;
+
+void onButtonDown(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context) 
+{
+    if (verbose) 
+    {
+        console() << "Button " << buttonID << " down on device " << device->deviceID << " at " << timestamp << " with context " << context << std::endl;
+    }
+}
+
+void onButtonUp(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context) 
+{
+    if (verbose) 
+    {
+        console() << "Button " << buttonID << " up on device " << device->deviceID << " at " << timestamp << " with context " << context << std::endl;
+    }
+}
+
+void onAxisMoved(struct Gamepad_device * device, unsigned int axisID, float value, float lastValue, double timestamp, void * context) 
+{
+    if (verbose) 
+    {
+        console() << "Axis " << axisID << " moved from " << lastValue <<" to " << value << " on device " << device->deviceID << " at " << timestamp << " with context " << context << std::endl;
+    }
+}
+
+void onDeviceAttached(struct Gamepad_device * device, void * context) 
+{
+    if (verbose) 
+    {
+        console() << "Device ID " << device->deviceID << " attached (vendor = " << device->vendorID <<"; product = " << device->productID << ") with context" << context << std::endl;
+    }
+}
+
+void onDeviceRemoved(struct Gamepad_device * device, void * context) 
+{
+    if (verbose) 
+    {
+        console() << "Device ID " << device->deviceID << " removed with context " << context << std::endl;
+    }
+}
+
+// We'll create a new Cinder Application by deriving from the AppBasic class
+class BasicApp : public AppBasic 
+{
+  public:
+    void setup();
+    void shutdown();
+    void keyDown( KeyEvent event );
+    void update();
+    void draw();
+};
+
+void BasicApp::setup()
+{
+    Gamepad_deviceAttachFunc(onDeviceAttached, (void *) 0x1);
+    Gamepad_deviceRemoveFunc(onDeviceRemoved, (void *) 0x2);
+    Gamepad_buttonDownFunc(onButtonDown, (void *) 0x3);
+    Gamepad_buttonUpFunc(onButtonUp, (void *) 0x4);
+    Gamepad_axisMoveFunc(onAxisMoved, (void *) 0x5);
+    Gamepad_init();
+}
+
+void BasicApp::shutdown()
+{
+    Gamepad_shutdown();
+}
+
+void BasicApp::keyDown( KeyEvent event )
+{
+    if (event.getCode() == KeyEvent::KEY_ESCAPE)
+    {
+        quit();
+    }
+}
+
+#define POLL_ITERATION_INTERVAL 30
+
+void BasicApp::update()
+{
+	static unsigned int iterationsToNextPoll = POLL_ITERATION_INTERVAL;
+
+    iterationsToNextPoll--;
+    if (iterationsToNextPoll == 0) {
+        Gamepad_detectDevices();
+        iterationsToNextPoll = POLL_ITERATION_INTERVAL;
+    }
+}
+
+void BasicApp::draw()
+{
+    Gamepad_processEvents();
+
+    gl::clear(ColorA(178/255.f,178/255.f,178/255.f,1.0f));
+}
+
+// This line tells Cinder to actually create the application
+CINDER_APP_BASIC( BasicApp, RendererGl )
